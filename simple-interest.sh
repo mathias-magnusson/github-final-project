825530e143462e5cf14f784af61e183dd5ca7f6d
mathias-magnusson <50197896+mathias-magnusson@users.noreply.github.com>
2023-06-17 11:14:27 +1000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # mmagdk@gmail.com
+   # mathias-magnusson
    # Input:
    # p, principal amount
    # t, time period in years
